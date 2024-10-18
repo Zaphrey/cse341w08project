cd79433ac60e4d1b21ef1e95cc45bc21aab9257d
Zachary Humphreys <zachary.humphreys32@gmail.com>
2024-10-18 14:35:13 -0400

    updated headers

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express, Request, Response, NextFunction } from "express";
 import defaultRouter from "@routes/index"
 import bodyParser from "body-parser";
 
 const app: Express = express();
 const port: String = process.env.PORT || "3000"
 
+app.use("/", (req: Request, res: Response, next: NextFunction) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Request-Method", "POST, GET, PUT, DELETE, OPTIONS");
+    res.setHeader("Access-Control-Request-Headers", "content-type");
+})
 app.use("/", bodyParser.json())
 app.use("/", defaultRouter)
 
